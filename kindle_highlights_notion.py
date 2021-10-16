f4fa570a0843e5e1dc4ea964338062dd9086218e
Pedro4064 <pedrohlcruz@gmail.com>
2021-10-16 15:48:13 -0300

    [REFACTORED] The whole program, now more configurable as the style and configuration in general.
    In preparation for the CLI program, we refactored the code to make it more
    modular and reusable, and to be more configurable trough config files.

--- kindle_highlights_notion.py
+++ kindle_highlights_notion.py
@@ -1,94 +1,170 @@
 from notion.client import NotionClient
-from notion.block import BulletedListBlock, DividerBlock, SubheaderBlock, QuoteBlock, TextBlock
+from notion.block import BulletedListBlock, DividerBlock,HeaderBlock , SubheaderBlock, SubsubheaderBlock, QuoteBlock, TextBlock, TodoBlock, PageBlock, CalloutBlock, ToggleBlock
 from kindle_highlights import Highlights
 
+from sys import platform
 import json
 import os 
 
+class NotionCredentialError(Exception):
+    pass
+
 class TimeCapsule:
 
-    def set_up_notion_credentials(self, token_v2:str, page_url:str) -> None:
-        
-        # Create the config file and store the inforation for future reference 
-        with open('notion.config', 'w') as config_file:
-            data = {'token_v2': token_v2, 'page_url':page_url}
-            config_file.write(json.dumps(data, indent=4))
+    def __init__(self):
+        # Set basic variables
+        self.config_file = 'config.json'
+        self.style_file  = 'style.json'
+        self.kindle_log  =  'kindle.log'
 
+        # Set variables for future comparison
+        self.logged_highlights = []
+        self.kindle_highlights = []
+        self.new_highlights = []
 
-    def load_notion_credentials(self) -> None:
-        
-        # try to read the config file to load the information, if it fails to open, most probably there is no file
+        # Load the necessary information
         try:
-            with open('notion.config', 'r') as config_file:
-                self.credentials = json.loads(config_file.read())
-
+            self.config_values = self.__load_file(self.config_file)
         except:
-            raise "[ERROR] Notion config file not Set Up"
+            self.__generate_config_file()
+        
+        try:
+            self.style_values = self.__load_file(self.style_file)
+        except:
+            self.__generate_style_file()
+        
+    def config(self,name:str, value:str) -> None:
+        # First and foremost we need to check if we the config file is already in our system
+        if (not self.__file_exists):
+            self.__generate_config_file()
 
+            data = self.__load_file(self.config_file)
+            data[name] = value 
+
+            self.__write_file(self.config_file, data)
+        
+        # If the config is already in the system, load it to change the data in it and save it back
+        else:
+            config_data = self.__load_file(self.config_file)
+            config_data[name] = value
+            self.__write_file(self.config_file, config_data)
+
+    def style(self,name:str, value:str) -> None:
+        # First and foremost we need to check if we the style file is already in our system
+        if (not self.__file_exists):
+            self.__generate_style_file()
+
+            data = self.__load_file(self.style_file)
+            data[name] = value
+
+            self.__write_file(self.style_file, data)
+        
+        # If the config is already in the system, load it to change the data in it and save it back
+        else:
+            style_data = self.__load_file(self.style_file)
+            style_data[name] = value
+            self.__write_file(self.config_file, style_data)
+
+    def __file_exists(self, name:str) -> bool:
+        if (name not in os.listdir()):
+            return False
+        
+        else:
+            return True
+        
+    def __generate_config_file(self) -> None:
+            
+        data = {'notion.v2token':'',
+                'notion.page':'',
+                'kindle.location': '/Volumes/Kindle' if platform=='darwin' else '/media/Kindle',
+                'kindle.log': self.kindle_log}
+
+        self.__write_file(file_name = self.config_file, data = data, overwrite=True)
+
+    def __generate_style_file(self) -> None:
+            
+        data = {'title block': {'color':'pink', 'block type':'SubheaderBlock'},
+                'quote block': {'color':'default', 'block type':'QuoteBlock'},
+                'annotation block': {'color':'gray', 'block type':'BulletedListBlock'}}
+
+        self.__write_file(file_name = self.style_file, data = data, overwrite=True)
+         
+    def __load_file(self, file_name:str) -> dict:
+        with open(file_name, 'r') as f:
+            return json.loads(f.read())
+
+    def __write_file(self, file_name:str, data:dict, overwrite:bool = True):
+            mode = 'w' if overwrite else 'a'
+
+            with open(file_name, mode) as f:
+                f.write(json.dumps(data, indent=4))
 
     def get_kindle_highlights(self, only_new:bool = True)->list:
-
+        
         # Get the data from the kindle .txt file 
         kindle = Highlights()
-        self.highlights = kindle.get_kindle_highlights()
+        self.kindle_highlights = kindle.get_kindle_highlights(self.config_values['kindle.location'])
 
-        # Read the log file
-        old_data = self.__load_last_kindle_data()
-
-
-        # Now that we have the data, we can save to the log for the next time before changing the variables
-        with open('kindle.log', 'w') as log_file:
-            log_file.write(json.dumps(self.highlights, indent=4))
+        # Read the log file, create an populate with empty json if does not exist
+        if (not self.__file_exists(self.kindle_log)):
+            self.logged_highlights = []
+            self.__write_file(self.kindle_log, self.logged_highlights)
+        else:
+            self.logged_highlights = self.__load_file(self.kindle_log)
 
         # If the argument `only_new` is set to true, we first need to parse the data before uploading to notion 
         # So we first need to load the data from the last kindle check
         if only_new:
-
             # only keep the new data during the list comprehention 
-            self.highlights = [highlight for highlight in self.highlights if highlight not in old_data] 
+            self.new_highlights = [highlight for highlight in self.kindle_highlights if highlight not in self.logged_highlights] 
 
             
-        # If they want all the data, simply don't change the self.highlights variable 
-        return self.highlights
+        return self.kindle_highlights if only_new else self.kindle_highlights
 
-
-    def upload_highlights(self, subcategories:bool = True)-> None:
+    def upload_highlights(self, highlights:list)-> None:
         
-        # To upload the highlights to notion we first need to initialize the notion API
+        # To upload the highlights to notion we first need to initialize the notion API, if it fails 
         self.__initialize_notion_api()
-        
-        # No that we have initialized, we can upload, but first we need to check rather the user wants to upload 
-        # the quotes in  subcategories (i.e all the quotes from one book as sublocks to the book title) or 
-        # just save the each quote with their own title on their own block.
-        if subcategories:
-            # Since the user chose to save in subcategories, we first need to separate each highlight by their book
 
-            # We first get all the book titles form each and every entry  
-            book_titles = [entry['book title'] for entry in self.highlights]
-
-            # Then we create a dictionary with the titles as keys (there will be no repetition) with the value of an empty list
-            ordered_highlights = dict.fromkeys(book_titles, [])
-            
-            # Now we can populate every list with a quote
-            for key in ordered_highlights:
-                ordered_highlights[key] = [entry for entry in self.highlights if entry['book title'] == key]
-            
-
-            # Now that we have the quotes separated by their book title, we can upload the data 
-            self.__populate_notion(ordered_highlights)
     
+        # First need to separate each highlight by their book
+        # We first get all the book titles form each and every entry  
+        book_titles = [entry['book title'] for entry in highlights]
+
+        # Then we create a dictionary with the titles as keys (there will be no repetition) with the value of an empty list
+        ordered_highlights = dict.fromkeys(book_titles, [])
+        
+        # Now we can populate every list with a quote
+        for key in ordered_highlights:
+            ordered_highlights[key] = [entry for entry in highlights if entry['book title'] == key]
+        
+
+        # Now that we have the quotes separated by their book title, we can upload the data 
+        self.__populate_notion(ordered_highlights)
+
+        # And at last we can save the data to the kindle file 
+        self.__write_file(self.kindle_log, self.kindle_highlights)
 
     def __initialize_notion_api(self):
-
-        self.client = NotionClient(token_v2 = self.credentials['token_v2'])
-        self.page = self.client.get_block(self.credentials['page_url'])
-
+        try:
+            self.client = NotionClient(token_v2 = self.config_values['notion.v2token'])
+            self.page = self.client.get_block(self.config_values['notion.page'])
+        except:
+            raise NotionCredentialError()
 
     def __populate_notion(self, highlights:dict):
         
+        # Set the types of blocks
+        types = {'HeaderBlock': HeaderBlock, 'SubheaderBlock': SubheaderBlock, 'SubsubheaderBlock': SubsubheaderBlock, 'QuoteBlock': QuoteBlock, 'TextBlock': TextBlock, 'PageBlock': PageBlock,'BulletedListBlock':BulletedListBlock ,'TodoBlock': TodoBlock, 'CalloutBlock': CalloutBlock, 'ToggleBlock': ToggleBlock}
+        
+        header_type = types[self.style_values['title block']['block type']]
+        location_type = types[self.style_values['annotation block']['block type']]
+        quote_type = types[self.style_values['quote block']['block type']]
+        
         # Set the color of the blocks 
-        header_color = 'pink'
-        location_color = 'gray'
+        header_color = self.style_values['title block']['color']
+        location_color = self.style_values['annotation block']['color']
+        quote_color = self.style_values['quote block']['color']
 
         # Get a list of all title already on the page 
         notion_titles = []
@@ -105,11 +181,18 @@ class TimeCapsule:
             # First we need to check rather or not the book already has an entry 
             if title not in notion_titles:
                 # Since there are no previous entries, we can simply add the title and the quote at the end of the page
-                self.page.children.add_new(SubheaderBlock, title=title, color=header_color)
+                header_block = self.page.children.add_new(header_type, title=title, color=header_color)
                 for quote in highlights[title]:
-                    self.page.children.add_new(QuoteBlock, title=quote['quote'])
-                    self.page.children.add_new(BulletedListBlock, title=quote['book location'].replace('-',''), color=location_color)
-                    self.page.children.add_new(DividerBlock)
+                    quote_block = self.page.children.add_new(quote_type, title=quote['quote'], color=quote_color)
+                    page_info_block = self.page.children.add_new(location_type, title=quote['book location'].replace('-',''), color=location_color)
+                    divider_block = self.page.children.add_new(DividerBlock)
+
+                    # Now, if the header block is either a page or a toggle, move the blocks to the correct location 
+                    if self.style_values['title block']['block type'] == 'ToggleBlock' or self.style_values['title block']['block type'] == 'PageBlock':
+                        quote_block.move_to(header_block, "last-child")
+                        page_info_block.move_to(header_block, "last-child")
+                        divider_block.move_to(header_block, "last-child")
+
 
                 # Add the titles to the notion_titles now that we have added it no notion 
                 notion_titles.append(title)
@@ -121,15 +204,14 @@ class TimeCapsule:
 
                 # Now that we have the parent block, we can iterate through the quotes and add them
                 for quote in highlights[title]:
-                    quote_block = self.page.children.add_new(QuoteBlock, title=quote['quote'])
-                    page_info_block = self.page.children.add_new(BulletedListBlock, title=quote['book location'].replace('-',''), color=location_color)
+                    quote_block = self.page.children.add_new(quote_type, title=quote['quote'], color=quote_color)
+                    page_info_block = self.page.children.add_new(location_type, title=quote['book location'].replace('-',''), color=location_color)
                     divider_block = self.page.children.add_new(DividerBlock)
 
                     # Now move the blocks to the correct location 
-                    quote_block.move_to(header_block, 'after')
-                    page_info_block.move_to(quote_block, 'after')
-                    divider_block.move_to(page_info_block, 'after')
-
+                    quote_block.move_to(header_block, "last-child")
+                    page_info_block.move_to(quote_block, "last-child")
+                    divider_block.move_to(page_info_block, "last-child")
 
     def __filter_children(self, child) -> bool:
 
@@ -140,23 +222,17 @@ class TimeCapsule:
             return False
 
 
-    def __load_last_kindle_data(self):
-        
-        try:
-            with open('kindle.log', 'r') as log_file:
-
-                data = json.loads(log_file.read())
-                return data
-
-        except:
-            return []
-
 
 if __name__ == '__main__':
-
+    
     vault = TimeCapsule()
-    # vault.set_up_notion_credentials(token_v2='****', page_url="*****")
-    vault.load_notion_credentials()
-
+    vault.config('notion.v2token', '5d94679b2acadf4d2e73318f270dd22924fed088b893852deebdc00a08edae3e0f88beae6d1ae32a02796f1b21bf9d0775bdfce4d1f0c4db2fc3e4d427b8e8cbab12e3c07b87207d6042f1860025')
+    # vault.config('notion.page', 'https://www.notion.so/a-724ac4ea9f054051b377749bb610677f')
     highlights = vault.get_kindle_highlights()
-    vault.upload_highlights()
+    vault.upload_highlights(highlights)
+
+    # # vault.set_up_notion_credentials(token_v2='****', page_url="*****")
+    # vault.load_notion_credentials()
+
+    # highlights = vault.get_kindle_highlights()
+    # vault.upload_highlights()
