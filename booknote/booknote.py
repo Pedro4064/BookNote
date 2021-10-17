d83a3e99cc7bff79f6ccd4798f5af888bd2002f2
Pedro4064 <pedrohlcruz@gmail.com>
2021-10-17 19:52:33 -0300

    [IMPLEMENTED] package safe way of referencing file

--- booknote/booknote.py
+++ booknote/booknote.py
@@ -3,6 +3,7 @@ from notion.block import BulletedListBlock, DividerBlock,HeaderBlock , Subheader
 from booknote.kindle_highlights import Highlights
 
 from sys import platform
+import pkgutil
 import json
 import os 
 
@@ -13,9 +14,9 @@ class TimeCapsule:
 
     def __init__(self):
         # Set basic variables
-        self.config_file = './booknote/config/config.json'
-        self.style_file  = './booknote/config/style.json'
-        self.kindle_log  =  './booknote/config/kindle.log'
+        self.config_file = pkgutil.get_data(__name__,'booknote/config/config.json')
+        self.style_file  = pkgutil.get_data('booknote/config/style.json')
+        self.kindle_log  =  pkgutil.get_data('booknote/config/kindle.log')
 
         # Set variables for future comparison
         self.logged_highlights = []
