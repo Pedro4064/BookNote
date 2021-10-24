a1a6a53e5540641c82851b201e07973898cd52b6
Pedro4064 <pedrohlcruz@gmail.com>
2021-10-24 12:30:14 -0300

    [ADDED] Doc string to the methods

--- booknote/booknote.py
+++ booknote/booknote.py
@@ -41,6 +41,17 @@ class TimeCapsule:
             self.__write_file(self.kindle_log, [])
         
     def config(self,name:str, value:str) -> None:
+        """Change the value for one of the configuration's variables. 
+        If config file not found it creates it in `~/.config/booknote` with the default values and than changes the desired one.
+
+        Args:
+            name (str): Name of the target config variable to be changed
+            value (str): The new Value
+
+        Returns:
+            None
+        """
+
         # First and foremost we need to check if we the config file is already in our system
         if (not self.__file_exists(self.config_file)):
             self.__generate_config_file()
@@ -57,6 +68,18 @@ class TimeCapsule:
             self.__write_file(self.config_file, config_data)
 
     def style(self,element:str, variable:str, value:str) -> None:
+        """Change the value for one of the notion's style config variables. 
+        If style file not found it creates it in `~/.config/booknote` with the default values and than changes the desired one.
+
+        Args:
+            element (str): The element it wants to change a style property of
+            variable (str): What property it wants to change
+            value (str): The new value for the property
+
+        Returns:
+            None
+        """
+
         # First and foremost we need to check if we the style file is already in our system
         if (not self.__file_exists(self.style_file)):
             self.__generate_style_file()
@@ -73,9 +96,22 @@ class TimeCapsule:
             self.__write_file(self.style_file, style_data)
 
     def __file_exists(self, path_to_file:str) -> bool:
+        """ Checks if a file exists in the user system
+
+        Args:
+            path_to_file (str)
+
+        Returns:
+            bool: `True` if the file exists, `False` otherwise
+        """
         return os.path.exists(path_to_file)
         
     def __generate_config_file(self) -> None:
+        """Generates a config.json file in the `self.config_file` location with all default values
+
+        Returns:
+            None
+        """
             
         data = {'notion.v2token':'',
                 'notion.page':'',
@@ -85,6 +121,11 @@ class TimeCapsule:
         self.__write_file(file_name = self.config_file, data = data, overwrite=True)
 
     def __generate_style_file(self) -> None:
+        """Generates a style.json file in the `self.style_file` location with all default values
+
+        Returns:
+            None
+        """
             
         data = {'title': {'color':'pink', 'block.type':'SubheaderBlock'},
                 'quote': {'color':'default', 'block.type':'QuoteBlock'},
@@ -93,16 +134,44 @@ class TimeCapsule:
         self.__write_file(file_name = self.style_file, data = data, overwrite=True)
          
     def __load_file(self, file_name:str) -> dict:
+        """ Load a json file
+
+        Args:
+            file_name (str): The path to the file
+
+        Returns:
+            dict: The contents of the json file
+        """
+
         with open(file_name, 'r') as f:
             return json.loads(f.read())
 
     def __write_file(self, file_name:str, data:dict, overwrite:bool = True) -> None:
-            mode = 'w' if overwrite else 'a'
+        """ Writes the formatted `data` to a json file.
 
-            with open(file_name, mode) as f:
-                f.write(json.dumps(data, indent=4))
+        Args:
+            file_name (str): Path to the file
+            data (dict): json data
+            overwrite (bool, optional): Rather it appends or overwrites the data on the file. Defaults to True (i.e overwrite).
+
+        Returns:
+            None
+        """
+
+        mode = 'w' if overwrite else 'a'
+
+        with open(file_name, mode) as f:
+            f.write(json.dumps(data, indent=4))
 
     def get_kindle_highlights(self, only_new:bool = True)->list:
+        """ Load and parse the `My Clippings.txt` file from the user's kindle. It can either return only the new entries or all of them
+
+        Args:
+            only_new (bool, optional): All entries or only the new ones. Defaults to True.
+
+        Returns:
+            list: Highlights from the user's kindle
+        """
         
         # Get the data from the kindle .txt file 
         kindle = Highlights()
@@ -125,6 +194,17 @@ class TimeCapsule:
         return self.kindle_highlights if only_new else self.kindle_highlights
 
     def upload_highlights(self, highlights:list)-> None:
+        """ Upload to Notion the `highlights` from the kindle
+
+        Args:
+            highlights (list): All the highlights to be uploaded
+
+        Raises:
+            NotionCredentialError: If there is an error with notion's authorization on sign-in
+
+        Returns:
+            None
+        """
         
         # To upload the highlights to notion we first need to initialize the notion API, if it fails 
         self.__initialize_notion_api()
@@ -149,6 +229,14 @@ class TimeCapsule:
         self.__write_file(self.kindle_log, self.kindle_highlights)
 
     def __initialize_notion_api(self) -> None:
+        """ Initialize the notion connection
+
+        Raises:
+            NotionCredentialError: If there is an error during authentication
+
+        Returns:
+            None
+        """
         try:
             self.client = NotionClient(token_v2 = self.config_values['notion.v2token'])
             self.page = self.client.get_block(self.config_values['notion.page'])
@@ -156,6 +244,14 @@ class TimeCapsule:
             raise NotionCredentialError()
 
     def __populate_notion(self, highlights:dict) -> None:
+        """ Populate the target Notion page with the `highlights` according to the styles specified in the `styles.json` file
+
+        Args:
+            highlights (dict)
+
+        Returns:
+            None
+        """
         
         # Set the types of blocks
         types = {'HeaderBlock': HeaderBlock, 'SubheaderBlock': SubheaderBlock, 'SubsubheaderBlock': SubsubheaderBlock, 'QuoteBlock': QuoteBlock, 'TextBlock': TextBlock, 'PageBlock': PageBlock,'BulletedListBlock':BulletedListBlock ,'TodoBlock': TodoBlock, 'CalloutBlock': CalloutBlock, 'ToggleBlock': ToggleBlock}
@@ -217,6 +313,14 @@ class TimeCapsule:
                     divider_block.move_to(page_info_block, "last-child")
 
     def __filter_children(self, child) -> bool:
+        """ Checks if the Notion block is the target one. To be used with the `filter` function.
+
+        Args:
+            child (Block): The target block
+
+        Returns:
+            bool: `True` if child's title equals desired title
+        """
 
         try:
             if child.title == self.title:
