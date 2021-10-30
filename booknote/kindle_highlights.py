1e9ae5f6b06b820d1e8b66602fa92b3d3e936a8b
Pedro4064 <pedrohlcruz@gmail.com>
2021-10-30 12:29:55 -0300

    [ADDED] Docstring

--- booknote/kindle_highlights.py
+++ booknote/kindle_highlights.py
@@ -1,11 +1,23 @@
 class Highlights:
     
     def get_kindle_highlights(self, kindle_path:str = '/Volumes/Kindle') -> list:
+        """Read and parse the Highlights from the user's kindle device
+
+        Args:
+            kindle_path (str, optional): The path to the mounter kindle device. Defaults to '/Volumes/Kindle'.
+
+        Returns:
+            list: The json formatted Highlights 
+        """        
         self.path = kindle_path
         return self.__parse_file()
 
     def __parse_file(self) -> list:
-        
+        """Parse and format the MyClippings.txt file from the kindle
+
+        Returns:
+            list: format the contents of the file in json
+        """            
         # Read the file that contains the kindle's highlights 
         with open(self.path+'/documents/My Clippings.txt', 'r') as data_file:
             data = data_file.read()
